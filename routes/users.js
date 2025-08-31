02f50b946a3da09476a6b414be4a467a84d6d3b2
Uday <raghavudaypratap7@gmail.com>
2025-08-31 20:00:53 +0530

    new commit

--- routes/users.js
+++ routes/users.js
@@ -1,49 +1,17 @@
-const mongoose = require('mongoose');
-const plm = require("passport-local-mongoose");
+const mongoose = require("mongoose");
+const passportLocalMongoose = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instainsta");
+mongoose.connect("mongodb://127.0.0.1:27017/instaclone");
 
 const userSchema = mongoose.Schema({
   username: String,
   name: String,
   email: String,
-  password: String,
-  picture: {
-    type: String,
-    default: "def.png"
-  },
-  contact: String,
+  profileImage: String,
   bio: String,
-  stories: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "story" 
-    }
-  ],
-  saved: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "post" 
-    }
-  ],
-  posts: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "post" 
-  }],
-  followers: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user" 
-    } 
-  ],
-  following: [
-    {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user" 
-    }
-  ]
-})
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
+});
 
-userSchema.plugin(plm);
+userSchema.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model("user", userSchema);
